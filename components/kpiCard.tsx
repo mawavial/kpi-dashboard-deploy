6f89cfd987c5b4ddf39ecf34907ccacd19d448ac
panzerkovic <panzerkovic@gmail.com>
2021-06-21 02:03:32 -0300

    improve typography

--- components/kpiCard.tsx
+++ components/kpiCard.tsx
@@ -2,15 +2,16 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
 import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 import React from "react"
 import IKpiData from "../interfaces/IKpiData";
 
 
-interface IProps { 
+interface IProps {
     data: IKpiData;
 }
 
-const KpiCard = (props:IProps) => {
+const KpiCard = (props: IProps) => {
 
     //call reducer and get kpi
 
@@ -18,19 +19,27 @@ const KpiCard = (props:IProps) => {
         <Container>
             <Card style={{
                 border: '2px solid #4907a0',
-                borderRadius:20,
-                backgroundColor:'#fff',
-                alignItems:'center',
+                borderRadius: 20,
+                backgroundColor: '#fff',
+                alignItems: 'center',
                 boxShadow: '8px 8px 0 2px #c0c0c0',
-                 }}>
+            }}>
                 <CardContent>
-                    {props.data.name}
+
+                    <Typography variant='h4' component='h1' style={{ color: '#556cd6' }} >
+                        {props.data.name}
+                    </Typography>
                 </CardContent>
                 <CardContent>
-                    {props.data.value}
+                    <Typography variant='h4' component='h1' style={{ color: '#556cd6' }} >
+                        {props.data.value}
+                    </Typography>
                 </CardContent>
                 <CardContent>
-                    {props.data.lastValue} Last Period
+                    <Typography variant='h4' component='h1' style={{ color: '#556cd6' }} >
+                        {props.data.lastValue} Last Period
+                    </Typography>
+
                 </CardContent>
             </Card>
         </Container>
